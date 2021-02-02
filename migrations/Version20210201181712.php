43487ac5876fbb30c523c818c2ace5ce5583d7e8
Obinna <obinna@fivenines.com.ng>
2021-02-02 14:26:19 +0100

    feed migration

---
+++ migrations/Version20210201181712.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210201181712 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE content ADD feed_id INT NOT NULL');
+        $this->addSql('ALTER TABLE content ADD CONSTRAINT FK_FEC530A951A5BC03 FOREIGN KEY (feed_id) REFERENCES feed (id)');
+        $this->addSql('CREATE INDEX IDX_FEC530A951A5BC03 ON content (feed_id)');
+        $this->addSql('ALTER TABLE feed ADD slug VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE content DROP FOREIGN KEY FK_FEC530A951A5BC03');
+        $this->addSql('DROP INDEX IDX_FEC530A951A5BC03 ON content');
+        $this->addSql('ALTER TABLE content DROP feed_id');
+        $this->addSql('ALTER TABLE feed DROP slug');
+    }
+}
